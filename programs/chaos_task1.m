f1daa9442a6d38430118b1f8fe309b9183881ab2
omidashtari <omid.ashtari@epfl.ch>
2025-04-24 20:16:10 +0200

    task1 script added.

---
+++ programs/chaos_task1.m
@@ -0,0 +1,29 @@
+close all; clear; clc
+
+%% inputs
+addpath('../functions/')       % folder containing functions
+L = 22.0;                      % domain length
+N = 64;                        % spatial resolution
+symm = false;                  % imposed center symmetry
+T_trans = 1000.0;              % transient time period
+T_study = 150.0;               % analysis time period
+dt = 0.1;                      % time step size for time integration
+dt_store = 1.0;                % time intervals of storing a snapshot
+epsilon = 1e-3;                % relative ampliture of perturbation
+
+%% initial condition
+[x,~] = domain(L,N);           % construct the spatial domain
+u0 = sin(2.0*pi*x/L) ...
+    + cos(4.0*pi*x/L);         % initial condition in physical state
+v0 = field2vector(u0,N,symm);  % initial state vector
+
+%% transient time integration
+[v1000,~] = KSE_integrate(v0,T_trans,dt,0,L,N,symm);
+
+%% perturbing the state vector
+r = zeros(size(v1000));
+for k = 1:length(r)
+    %%% to be completed
+end
+
+%%% to be completed
