172a9bee8d794547c2a739bffbd1555a1afd504f
omidashtari <omid.ashtari@epfl.ch>
2025-04-26 19:45:42 +0200

    task2 modified.

--- programs/chaos_task2.m
+++ programs/chaos_task2.m
@@ -2,9 +2,9 @@ close all; clear; clc
 
 %% inputs
 addpath('../functions/')       % folder containing functions
-L = 22.0;                      % domain length
+L = 38.6;                      % domain length
 N = 64;                        % spatial resolution
-symm = false;                  % imposed center symmetry
+symm = true;                   % imposed center symmetry
 dt = 0.1;                      % time step size for time integration
 epsilon = 1e-6;                % perturbation amplitude for computing J
 tau = 2;                       % time intervals of computing J
@@ -13,8 +13,7 @@ N_exp = 10;                    % number of Lyapunov exponents
 
 %% initial condition
 [x,~] = domain(L,N);           % construct the spatial domain
-u0 = sin(2.0*pi*x/L) ...
-    + cos(4.0*pi*x/L);         % initial condition in physical state
+u0 = sin(2.0*pi*x/L);          % initial condition in physical state
 v0 = field2vector(u0,N,symm);  % initial state vector
 
 %% compute Lyapunov exponents
